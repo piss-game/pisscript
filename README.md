8a9507dbe2e979a822ea17893aa8099c3fbb4fbe
oldpepper12 <oldpepper12@gmail.com>
2021-06-30 00:15:51 -0400

    alpha version

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # pisscript
-A scripting language involving piss
+
+*A next-gen scripting language designed for modern software development*
+
+## Overview
+
+Pisscript, at its core, is a scripting language about bodily fluids. If, as it may be for many immature programmers,
+this is too much to handle, you are welcome to choose a different language.
+
+## `Hello World`
+
+```python
+ejaculate "Hello, World!"
+```
+
+Keep in mind the careful use of the word *ejaculate* in this context. An entirely valid, albiet slightly outdated use of the word *ejaculate* means to "say something quickly and suddenly". In fact, the word was used in this context in Chapter 29 of the popular science-fiction novel *Harry Potter and the Half-Blood Prince* by J.K Rowling.
+
+## Tubs
+
+There are no variables in Pisscript, rather there are Tubs. Tubs can contain Piss or they can contain Words. The two are mutually exclusive. In layman's terms, Tubs can contain either numbers or strings. No distinction is drawn between integers and floating point numbers.
+
+The existence of a tub with negative quantities of Piss will induce the creation of antimatter-Piss, a.k.a. Antipiss. Such an event will result in the violent anihilation of the Antipiss with the surrounding environment resulting in the destruction of the Earth as well as a segmentation fault.
+
+## Arithmetic
+
+Here is a program that adds two numbers together. The workings of this program should be fairly self-explanatory.
+
+```python
+tub a
+fill a with 1
+
+tub b
+fill b with 2
+
+pour b into a
+
+tub message
+fill message with "1 + 2 = "
+
+measure a
+stick a onto message
+
+ejaculate message
+```
+
+## Installation and Usage
+
+To install: do a `git clone`
+To run: `python3 pisscript.py [input file]`
+
---
+++ pisscript.py
@@ -0,0 +1,275 @@
+"""
+=== Pisscript ===
+
+An esoteric programming language designed around the environment of
+the household restroom.
+
+"""
+
+import re
+import sys
+
+### UTILITY ###
+
+class PSError(Exception):
+    def __init__(self, msg, ln):
+        super().__init__(msg)
+        self.msg = msg
+        self.ln = ln
+
+class PSSyntaxError(PSError):
+    pass
+
+class PSRuntimeError(PSError):
+    pass
+
+class Source:
+    def __init__(self, text, source_file):
+        self.text = text
+        self.source_file = source_file
+
+def load_source(path):
+    with open(path, 'r') as fl:
+        return Source(fl.read(), path)
+
+def segfault():
+    import ctypes;ctypes.string_at(0)
+
+### PARSING ###
+
+class TokenStream:
+    def __init__(self, source):
+        self.source = source
+        self.char_index = 0
+        self.ln = 1
+
+    def has_reached_end(self):
+        return self.char_index == len(self.source.text)
+    
+    def _next_token(self):
+        if self.has_reached_end():
+            return Token("eof", "", self.ln)
+
+        next_string = self.get_curr_string()
+
+        token_patterns = {
+            r'[0-9]+': "number",
+            r'tub': "verb",
+            r'fill': "verb",
+            r'pour': "verb",
+            r'ejaculate': "verb",
+            r'measure': "verb",
+            r'stick': "verb",
+            r' ': "no_token",
+            r'\n': "endl",
+            r'".+?"': "string",
+            r'[a-zA-Z_][a-zA-Z_0-9]{0,}': "name",
+        }
+
+        for pattern, token_type in token_patterns.items():
+            match_obj = re.match(pattern, next_string)
+
+            if match_obj:
+                match = match_obj[0]
+                self._consume_string(match)
+
+                # quick sanity check
+                if token_type == "string" and "\n" in match:
+                    raise PSSyntaxError("Unterminated string literal", self.ln)
+
+                # end
+                return Token(token_type, match, self.ln)
+
+        raise PSSyntaxError(f"Unexpected token {repr(next_string[0])}", self.ln)
+
+    def _consume_string(self, string):
+        for i in range(len(string)):
+            char = self._next_char()
+
+            if char == "\n":
+                self.ln += 1
+
+    def next_token(self):
+        token = self._next_token()
+
+        while token.type == "no_token":
+            token = self._next_token()
+
+        return token
+
+    def _next_char(self):
+        char = self.source.text[self.char_index]
+        self.char_index+=1
+        return char
+
+    def _peek_char(self):
+        return self.source.text[self.char_index]
+    
+    def get_curr_string(self):
+        return self.source.text[self.char_index:]
+
+class Token:
+    def __init__(self, type, content, line):
+        self.type = type
+        self.content = content
+        self.line = line
+
+    def __repr__(self):
+        return f'<Token type={repr(self.type)} content={repr(self.content)}>'
+    
+    def is_eof(self):
+        return self.type == "eof"
+
+class Statement:
+    def __init__(self, verb, args, line):
+        self.verb = verb
+        self.args = args
+        self.line = line
+    
+    def get_arg(self, n):
+        if n >= len(self.args):
+            return None
+        return self.args[n]
+
+class Parser:
+    def __init__(self, source):
+        self.source = source
+        self.stream = TokenStream(source)
+        self.instructions = []
+    
+    def parse(self):
+        while not self.stream.has_reached_end():
+            self._parse_statement()
+    
+    def _parse_statement(self):
+        verb = self.stream.next_token()
+        line = self.stream.ln
+
+        # statement is blank line
+        if verb.type == "endl":
+            return
+        
+        if verb.type != "verb":
+            raise PSSyntaxError(f"Expected statement, found token {repr(verb.content)}", self.stream.ln)
+        
+        # get args (up until ENDL or EOF)
+
+        args = []
+
+        while True:
+            token = self.stream.next_token()
+
+            if token.type == "endl" or token.type == "eof":
+                break
+
+            args.append(token)
+            
+        self.instructions.append(Statement(verb.content, args, line))
+
+### RUNTIME ###
+
+class Tub:
+    def __init__(self, name):
+        self.type = "number"
+        self.value = 0
+    
+    def set_val(self, value):
+        if type(value) == str:
+            self.type = "string"
+        else:
+            self.type = "number"
+        
+        self.value = value
+
+        if self.type == "number":
+            if self.value < 0:
+                segfault()
+
+class PisscriptRuntime:
+    def __init__(self, source):
+        self.source = source
+        self.parser = Parser(source)
+        self.tubs = {}
+        self.instructions = []
+        self.callstack = []
+        self.program_counter = 0
+        self.ln = 0
+    
+    def add_tub(self, name):
+        if name in self.tubs:
+            self.throw_runtime_err(f"Tub with name {repr(name)} already exists!")
+        
+        self.tubs[name] = Tub(name)
+
+    def throw_runtime_err(self, msg):
+        raise PSRuntimeError(msg, self.ln)
+    
+    def run(self):
+        error_type = None
+        error = None
+        try:
+            self._run()
+        except PSRuntimeError as err:
+            error_type = "Runtime Error"
+            error = err
+        except PSSyntaxError as err:
+            error_type = "Syntax Error"
+            error = err
+        finally:
+            if error_type:
+                print(f"\033[91m{self.source.source_file}:{error.ln}")
+                print(f"{error_type}: {error.msg}")
+                print(" | "+self.source.text.split("\n")[error.ln-1].strip())
+                print("   ^^^^")
+                print("that's kinda mungus moment if u ask me ඞ\033[0m")
+    
+    def _assert_token_type(self, token, type):
+        if token.type != type:
+            self.throw_runtime_err(f"Expected token of type {repr(type)}, found token {repr(token.content)}")
+
+    def _run(self):
+        self.parser.parse()
+        self.instructions = self.parser.instructions
+        
+        while self.program_counter < len(self.instructions):
+            inst = self._load_curr_instruction()
+            self.ln = inst.line
+
+            if inst.verb == "tub":
+                name_token = inst.get_arg(0)
+                if name_token is None:
+                    self.throw_runtime_err("Expected Tub name")
+                
+                self._assert_token_type(name_token, "name")
+
+                self.add_tub(name_token.content)
+
+            self.program_counter+=1
+
+    def _load_curr_instruction(self):
+        return self.instructions[self.program_counter]
+
+    def _jmp(self, to):
+        self.program_counter = to
+
+    def _return(self):
+        pass
+
+
+### TESTING ###
+
+if __name__=="__main__":
+    if len(sys.argv) < 2:
+        print("Error: no input files specified")
+        quit()
+
+    input_file = sys.argv[1]
+    src = load_source(input_file)
+    runtime = PisscriptRuntime(src)
+
+    runtime.run()
+
+
+
+
+
